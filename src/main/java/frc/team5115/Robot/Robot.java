6118000ee43f957fe380908f79789a4c56490deb
Travis CI <travis@travis-ci.org>
2020-01-08 15:39:12 -0500

    first commit to olivia

---
+++ src/main/java/frc/team5115/Robot/Robot.java
@@ -0,0 +1,32 @@
+package frc.team5115.Robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
+
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
