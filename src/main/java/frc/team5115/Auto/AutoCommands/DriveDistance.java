ee9435d4730748adce144c7313438a6039c0170d
dime35 <dime35.gd@gmail.com>
2020-01-09 15:10:35 -0500

    Actually fixed imports for Loc2D

--- src/main/java/frc/team5115/Auto/AutoCommands/DriveDistance.java
+++ src/main/java/frc/team5115/Auto/AutoCommands/DriveDistance.java
@@ -4,8 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team5115.Subsystems.Drivetrain;
 import frc.team5115.Subsystems.Locationator;
-import frc.team5115.autotools.Loc2D;
-
+import frc.team5115.Auto.Loc2D;
 public class DriveDistance extends CommandBase {
 
     Loc2D loc2D;
